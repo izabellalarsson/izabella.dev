c095e373967c87e31dfc8d0884b8bdba856232a3
Izabella Larsson Polestar <izabella.larsson@polestar.com>
2023-10-07 13:41:48 +0200

    feat: add prereact and say hello

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}: {messages: string[]}) {
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+console.log(Math.random() * messages.length)
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
