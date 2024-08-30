db33bc57917ecff43696e398392a09c0b2c11b35
Izabella Larsson <izabella.larsson@stampen.com>
2024-08-30 15:04:24 +0200

    fix: rss stream

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
     title: 'Izabella | Blog',
     description: 'Building my site',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./blog/*.md')),
     customData: `<language>en-us</language>`,
   });
 }
